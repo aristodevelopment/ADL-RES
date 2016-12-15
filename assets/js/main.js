724ca590de034699d538e4cebacbae18154be7ed
aristodevelopment <pmoss@aristodevelopment.com>
2016-12-14 23:54:41 -0500

    Front=end Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,6 +184,15 @@
 
 				});
 
+				//Scroll Down
+
+				$(function() {
+	$('a[href*=#]').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+	});
+});
+
 		// Features.
 			if (skel.canUse('transition'))
 				$('.features')
